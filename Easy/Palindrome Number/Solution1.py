799d550af11ec12c37e8cb6b1d38775b5ca42de8
TheUnknown550 <mattcosh06@gmail.com>
2025-09-11 02:05:55 +0700

    Palinedrome

--- Easy/Palindrome Number/Solution1.py
+++ Easy/Palindrome Number/Solution1.py
@@ -1,11 +1,24 @@
-x = 121
+x = 1211
 
 import math
 
+#Check if value is Negative
+if(x<0):
+    print(False)
+
+# convert x into a array of intergers O(n)
 num = []
 while (x > 0):
     print(x)
     num.append(x%10)
     x = math.floor(x/10)
     
-print(num)
+print(num) # check if array is complete
+
+#find palinedrome by comparing last and first values O(n)
+for i in range(math.ceil(len(num)/2)):
+    lastIndex = (len(num)-1)-i #find the last index - i
+    if(num[i] != num[lastIndex]):
+        print("False")
+        
+print("True")
