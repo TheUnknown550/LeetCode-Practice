a7e51d30ce05811ddebfc17347a5a7ff73ca3dec
TheUnknown550 <mattcosh06@gmail.com>
2025-10-05 04:26:45 +0700

    Init file

---
+++ Easy/27.Remove Element/RemoveElement.py
@@ -0,0 +1,14 @@
+from typing import List
+
+def removeElement(self, nums, val):
+
+    return nums
+
+def main():
+    nums = [3,2,2,3]
+    val = 3
+    print(removeElement(nums, val))
+
+if __name__ == "__main__":
+    main()
+    
