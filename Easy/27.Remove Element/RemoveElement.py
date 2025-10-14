e13dde1ab60cc32b72f49203a4f8541c8dc02741
TheUnknown550 <mattcosh06@gmail.com>
2025-10-14 21:04:58 +0700

    Update RemoveElement.py

--- Easy/27.Remove Element/RemoveElement.py
+++ Easy/27.Remove Element/RemoveElement.py
@@ -1,11 +1,18 @@
 from typing import List
 
-def removeElement(self, nums, val):
-
+def removeElement(nums, val):
+    for i, n in enumerate(nums):
+        if n == val:
+            if i + 1 < len(nums) and nums[i+1] != n:
+                nums[i] = nums[i+1]
+                nums[i+1] = n
+                print(nums[i], nums[i+1])
+            else:
+                nums[i] = 0
     return nums
 
 def main():
-    nums = [3,2,2,3]
+    nums = [3,2,2,3,2]
     val = 3
     print(removeElement(nums, val))
 
